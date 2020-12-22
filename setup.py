e26c771c8220a2a7513bbfaf73ccd95c0f2fe3eb
ravigoel08 <ravigoel.1997@gmail.com>
2020-12-23 00:26:05 +0530

    updated manifest, setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
   author = 'CodewithRv',                   # Type in your name
   author_email = 'ravigoel.1997@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/ravigoel08/pyzipcin',   # Provide either the link to your github or to your website
-  # download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/ravigoel08/pyzipcin/dist/pyzipcin-0.2.2.tar.gz',    # I explain this later on
   keywords = ['india', 'zipcodes', 'pincode','data.gov.in', 'pyzipcin'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'sqlalchemy',
